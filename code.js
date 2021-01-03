cf4fdee11e8c6cf3d3f6c60af98a934e2c5eb4a2
Ahmad-Alkhalaf <76259161+Ahmad-Alkhalaf@users.noreply.github.com>
2021-01-03 04:42:47 +0300

    cw5

---
+++ code.js
@@ -0,0 +1,7 @@
+let age = 21
+console.log(age)
+let agee = console.log(age*5)
+alert("حين أبلغ 5 أضعاف عمري سيبدأ زحل باستقبال المبرمجين على سطحه")
+let numberofYears = 2021-1969
+alert(`${numberofYears} هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر `)
+// اجابة السؤال الأول
